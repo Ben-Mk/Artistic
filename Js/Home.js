bd57ef7f1abc3c5f7599605dae4cc050246455d8
Ben-Mk <benmk38@gmail.com>
2023-12-20 01:50:23 +0300

    navbar mobile

--- Js/Home.js
+++ Js/Home.js
@@ -2,6 +2,18 @@ const humbergerMenu = document.querySelector(".nav-bar-links");
 const humbergerBtn = document.querySelector(".nav-bar-humberger-btn");
 const humbergerIcon = document.getElementsByTagName("i");
 
+const navigationBar = document.querySelector(".nav");
+const scrollWatcher = document.createElement("div");
+
+scrollWatcher.setAttribute("data-scroll-watcher", "");
+navigationBar.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver((entries) => {
+  navigationBar.classList.toggle("sticking", !entries[0].isIntersecting);
+});
+
+navObserver.observe(scrollWatcher);
+
 humbergerBtn.addEventListener("click", () => {
   if (humbergerMenu.classList.contains("appear")) {
     humbergerMenu.classList.remove("appear");
